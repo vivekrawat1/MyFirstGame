b0b219d47f7a6eb48d89fd08e4a1ecfdc2a60eab
vivekRawat <vivek.rawat49@gmail.com>
2017-03-29 23:31:40 +0530

    created tetris prefabs

--- assets/Scriipt/MainScript.js
+++ assets/Scriipt/MainScript.js
@@ -2,25 +2,36 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
+
+        tetrisModel1: {
+            default: null,
+            type: cc.Prefab
+        },
+        tetrisModel2: {
+            default: null,
+            type: cc.Prefab
+        },
+        tetrisModel3: {
+            default: null,
+            type: cc.Prefab
+        },
+        tetrisModel4: {
+            default: null,
+            type: cc.Prefab
+        },
+        rotation:{
+            default: [],
+            type: [cc.Integer]
+        }
     },
 
     // use this for initialization
     onLoad: function () {
-        console.log("hurrrreeeeeeeeeeeeeeeeeeeeeeeh");
+
     },
 
     // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
+    // update: function (dt) {et
 
     // },
 });
